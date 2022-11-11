f08481cc0a6f411c3fd7bfff40f60097caae45e2
Agustín Rossi <agustinrossituc@gmail.com>
2022-11-11 18:29:53 -0300

    lista la lógica para pintar las noticias en el index

--- js/pintarNoticias.js
+++ js/pintarNoticias.js
@@ -1,1 +1,22 @@
 const noticias = JSON.parse(localStorage.getItem("noticias")) || [] 
+const noticiasPintadas = document.getElementById("noticiasSection")
+
+function noticiasPintadasHTML() {
+    noticiasSection.innerHTML= ""
+    noticias.forEach((notic) => {
+        noticiasSection.innerHTML += 
+            `<div class="col-lg-6">
+            <!-- Blog post-->
+            <div class="card mb-4">
+                <a href="#!"><img class="card-img-top" src="${notic.imagen}" alt="${notic.título}" /></a>
+                <div class="card-body">
+                    <div class="small text-muted">6 Noviembre, 2022</div>
+                    <h2 class="card-title h4">${notic.título}</h2>
+                    <p class="card-text">${notic.descripción}</p>
+                    <a class="btn btn-primary" href="#!">Leer mas →</a>
+                </div>
+            </div>`
+    });
+}
+
+noticiasPintadasHTML()
